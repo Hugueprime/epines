ef368c8e06902f08383fb5bc4fd5aa3095d283bf
hugueprime <hugueprime@users.noreply.github.com>
2022-10-15 17:26:54 +0200

    some work on v3

--- background/background.js
+++ background/background.js
@@ -1,6 +1,10 @@
+import * as t from "/global/browser-polyfill.js"
+console.log("hello back")
+
 const filter = {
     urls: [
-        "*://www.youtube.com/*"
+        "*://www.youtube.com/*",
+        "https://courses.ionisx.com/*"
     ],
 };
 const webRequestFlags = [
@@ -11,25 +15,47 @@ function onError(error) {
     console.log(`Error: ${error}`);
 }
 
-function blockRequest(result) {
-    function blockYt(page) {
+// function blockRequest(result) {
+//     function blockYt(page) {
+//         browser.storage.local.get("isVideoPlayerActive").then(res => {
+//             //media player is not enabled
+//             if (!res.isVideoPlayerActive) {
+//                 return;
+//             }
+
+//             const instance = result.instanceVideoPlayer || "yewtu.be";
+//             return { cancel: page.url.includes("courses.ionisx.com") && !instance.includes("www.youtube.com") };
+//         });
+//     }
+
+//     browser.webRequest.onBeforeRequest.addListener(page => { return blockYt(page) },
+//         filter,
+//         webRequestFlags
+//     );
+// };
+
+function blockRequest2(result) {
+    
+
+    browser.webRequest.onBeforeRequest.addListener(page => {
+        console.log(page)
         browser.storage.local.get("isVideoPlayerActive").then(res => {
             //media player is not enabled
             if (!res.isVideoPlayerActive) {
                 return;
             }
-
+            
             const instance = result.instanceVideoPlayer || "yewtu.be";
-            return { cancel: page.url.includes("courses.ionisx.com") && !instance.includes("www.youtube.com") };
+            if (page.url.includes("courses.ionisx.com") && !instance.includes("www.youtube.com")) {
+                console.log("block")
+                chrome.declarativeNetRequest.updateEnabledRulesets({"disableRulesetIds": "ruleset_1"})
+            } else {
+                chrome.declarativeNetRequest.updateEnabledRulesets({"enableRulesetIds": "ruleset_1"})
+            }
         });
-    }
-
-    browser.webRequest.onBeforeRequest.addListener(page => { return blockYt(page) },
-        filter,
-        webRequestFlags
-    );
-};
+    }, filter);
+}
 
 browser.storage.local.get("instanceVideoPlayer").then(result => {
-    blockRequest(result);
+    blockRequest2(result);
 });
