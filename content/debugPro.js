fa3ab818601ff598a1366106312db76a190af30e
Tim-xD <74541981+Tim-xD@users.noreply.github.com>
2022-11-07 16:50:42 +0000

    Update debugPro.js

--- content/debugPro.js
+++ content/debugPro.js
@@ -31,7 +31,7 @@ function parseTest(s) {
     result = ""
     for (let i = 0; i < s.length; i++) {
         if (s[i][0] == '$' && s[i][1] == ' ') {
-            result += "echo \"" + s[i].substring(2) + "\" && " + s[i].substring(2) + " && ";
+            result += "echo \"" + s[i] + "\" && " + s[i].substring(2) + " && ";
         }
     }
 
