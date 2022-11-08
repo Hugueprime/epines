7b72aba4e991275f9d279398ca119d1cc7af73a2
hugueprime <hugueprime@users.noreply.github.com>
2022-11-08 13:02:23 +0000

    add make.sh

---
+++ make.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $1 = 'zip' ]; then
+    zip -r epines.zip background content global icons popup styles manifest.json
+fi
+
+if [ $1 = 'clean' ]; then
+    rm epines.zip -f
+fi
+
+if [ $1 = 'update' ]; then
+    sed -i 's%v[0-9\.]*</span>%v'$2'</span>%g' popup/popup.html
+    sed -i 's%\"version\": \"[0-9\.]*\"%\"version\": \"'$2'\"%g' manifest.json
+fi
+
+if [ $1 = 'version' ]; then
+    grep -G "v[0-9\.]*</span>" popup/popup.html
+fi
