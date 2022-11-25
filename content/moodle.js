625be9f954c0d234f1f6e0794dc5cdc3a37fdc81
hugueprime <hugueprime@users.noreply.github.com>
2022-11-25 19:05:04 +0100

    moodle: hide content after like

--- content/moodle.js
+++ content/moodle.js
@@ -4,14 +4,47 @@ function isMoodleUrl() {
 }
 
 function mainMoodle() {
-    console.log("in");
+    
     if (url.substring(0, 39) == "https://moodle.cri.epita.fr/mod/assign/") {
-        let elt = document.getElementsByClassName("feedback")[0]?.getElementsByClassName("plugincontentsummary")[0];
-        console.log(elt)
-        if (elt) {
-            console.log(makeLinkClickable(elt.textContent));
-            elt.innerHTML = makeLinkClickable(elt.textContent);
-        }
+        makeLinkClickableFeedback();
+    }
+    
+    if (url.substring(0, 34) == "https://moodle.cri.epita.fr/course") {
+        makeContentAfterLinkOptional();
     }
 }
 
+function makeContentAfterLinkOptional() {
+    let descriptions = document.getElementsByClassName("contentafterlink");
+    for (let i = 0; i < descriptions.length; i++) {
+        descriptions[i].classList.toggle("epines-hide");
+        const div = document.createElement("div");
+        div.classList.add("epines-line");
+        const divTxt = document.createElement("div");
+        divTxt.classList.add("epines-line-txt");
+        divTxt.textContent = "More informations";
+        div.appendChild(divTxt);
+        descriptions[i].parentElement.insertBefore(div, descriptions[i]);
+        
+        divTxt.addEventListener("click", () => {
+            moodleSeeMore(div, descriptions[i])
+        });
+    }
+}
+
+function moodleSeeMore(e, target) {
+    if (target.classList.contains("epines-hide")) {
+        e.firstChild.textContent = "Less";
+    } else {
+        e.firstChild.textContent = "More informations";
+    }
+    target.classList.toggle("epines-hide");
+}
+
+function makeLinkClickableFeedback() {
+    let elt = document.getElementsByClassName("feedback")[0]?.getElementsByClassName("plugincontentsummary")[0];
+    if (elt) {
+        console.log(makeLinkClickable(elt.textContent));
+        elt.innerHTML = makeLinkClickable(elt.textContent);
+    }
+}
