6b4411a791f63bb47f1e24aed116fa122035ad62
hugueprime <hugueprime@users.noreply.github.com>
2024-02-24 16:15:54 +0100

    fix everything

--- global/tools.js
+++ global/tools.js
@@ -41,6 +41,47 @@ function updateDates(force = false) {
     });
 }
 
+function updateRedirect(force = false) {
+    return new Promise(function(resolve, reject) {
+        const today = new Date();
+        browser.storage.local.get('redirectLastCheck').then(dateUp => {
+            if (force ||
+                Object.keys(dateUp).length == 0 ||
+                createDate(dateUp.redirectLastCheck) == "Invalid Date" ||
+                createDate(dateUp.redirectLastCheck).getDate() + 7 < today.getDate()
+            ) { //sync if last is one week old or doesn't exist
+                browser.storage.local.get('URLRedirect').then(url => {
+                    if (!url.URLRedirect)
+                        url.URLRedirect = DEFAULT_BOOKMARKS;
+                    fetch(url.URLRedirect, {cache: "no-cache"}).then(r => r.text()).then(result => {
+                        result = JSON.parse(result);
+                        if (result) {
+                            addToLocalStorage('redirectLastCheck', today.toLocaleDateString("fr-FR", {day: 'numeric', month: 'numeric'}));
+                            browser.storage.local.get('redirectLastVersion').then(version => {
+                                if (!version.redirectLastVersion || result.version > version.redirectLastVersion) {
+                                    addToLocalStorage('redirectLastVersion', result.version);
+                                    clearLocalStorage("bookmarks");
+                                    addToLocalStorage("bookmarks", JSON.stringify(result.bookmarks));
+                                    resolve("Updated")
+                                } else {
+                                    reject("Already on latest");
+                                }
+                            });
+                        } else {
+                            reject("Fetch failed")
+                        }
+                    }).catch(() => {
+                        reject("Failed to request")                
+                    });
+                    
+                });
+            } else {
+                resolve("No sync need")
+            }
+        });
+    });
+}
+
 function createDate(dateStr) { // dd/mm
     const day = dateStr.split('/')[0];
     const month = dateStr.split('/')[1]+1;
